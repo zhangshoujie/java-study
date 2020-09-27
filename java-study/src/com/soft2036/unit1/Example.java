407b7277ba5dac34ab32681eab548531d686df61
zhangshoujie <1753141460@qq.com>
2020-09-27 15:03:02 +0800

    9.27submit

---
+++ java-study/src/com/soft2036/unit1/Example.java
@@ -0,0 +1,23 @@
+package com.soft2036.unit1;
+
+/**
+ * @author 张守杰
+ */
+class Example1{
+    public static void main(String[] args) {
+        Rect rectangle;
+        rectangle  = new Rect();
+        rectangle.width = 1.819;
+        rectangle.height = 1.5;
+        double area = rectangle.getArea();
+        System.out.println("矩形的面积:" + area);
+    }
+}
+class Rect {
+    double width;
+    double height;
+    double getArea(){
+        return width * height;
+    }
+}
+
