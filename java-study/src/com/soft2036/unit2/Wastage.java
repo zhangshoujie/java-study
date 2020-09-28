051a03b87708556423a4e1184d951240b7478c01
zhangshoujie <1753141460@qq.com>
2020-09-28 10:59:35 +0800

    9.28submit

---
+++ java-study/src/com/soft2036/unit2/Wastage.java
@@ -0,0 +1,21 @@
+package com.soft2036.unit2;
+
+public class Wastage {
+    public static void main(String[] args) {
+        byte b = 22;
+        int n = 129;
+        float f = 123456.6789f;
+        double d = 123456789.123456789;
+        System.out.println("b = " + b);
+        System.out.println("n = " + n);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        b = (byte)n;
+        f = (float)d;
+        System.out.println("b = " + b);
+        System.out.println("f = " + f);
+    }
+
+
+
+}
