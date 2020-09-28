9181474805495b619dd5f0d7f31657cabc5483eb
zhangshoujie <1753141460@qq.com>
2020-09-29 07:50:53 +0800

    9.29submit

---
+++ java-study/src/com/soft2036/unit2/Quote.java
@@ -0,0 +1,22 @@
+package com.soft2036.unit2;
+
+/**
+ * @author 张守杰
+ */
+public class Quote {
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4};
+        int b[] = {100,200,300};
+        System.out.println("数组a的元素个数 = " + a.length);
+        System.out.println("数组b的元素个数 = " + b.length);
+        System.out.println("数组a的引用 = " + a);
+        System.out.println("数组b的引用 = " + b);
+        System.out.println("a == b的结果是" + (a == b));
+        a = b;
+        System.out.println("数组a的元素个数 = " + a.length);
+        System.out.println("数组b的元素个数 = " + b.length);
+        System.out.println("a == b的结果是" + (a == b));
+        System.out.println("a[0] = " + a[0] + ",a[1] =" + a[1] + ",a[2] =" + a[2]);
+        System.out.println("b[0] = " + a[0] + ",b[1] = " + b[1] + ",b[2] =" + b[2]);
+    }
+}
