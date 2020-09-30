b96d9ae1a0e1e0cc7264a089d4d49870e839329f
zhangshoujie <1753141460@qq.com>
2020-09-30 15:33:26 +0800

    9.30submit

---
+++ java-study/src/com/soft2036/unit3/Round.java
@@ -0,0 +1,22 @@
+package com.soft2036.unit3;
+/**
+ * @author 张守杰
+ */
+public class Round {
+    public static void main(String[] args) {
+        int match = 65,english = 85;
+        if (match > 60){
+            System.out.println("数学及格了");
+        }
+        else{
+            System.out.println("数学没及格");
+        }
+        if (english > 90){
+            System.out.println("英语及格了");
+        }
+        else{
+            System.out.println("英语没及格");
+        }
+        System.out.println("我在学习if-else语句");
+    }
+}
