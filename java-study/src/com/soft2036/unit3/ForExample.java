58f230d782d05363e9b339307973bfa995e3a0d3
zhangshoujie <1753141460@qq.com>
2020-09-30 21:10:25 +0800

    9.30submit

---
+++ java-study/src/com/soft2036/unit3/ForExample.java
@@ -0,0 +1,12 @@
+package com.soft2036.unit3;
+
+public class ForExample {
+    public static void main(String[] args) {
+        long sum = 0,a = 8,item = a,n = 12,i = 1;
+        for (i = 1;i <= n;i++){
+            sum = sum + item;
+            item = item * 10 + a;
+        }
+        System.out.println(sum);
+    }
+}
