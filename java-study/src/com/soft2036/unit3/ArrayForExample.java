f846819f65a7ae7904b1e68cb91f489b341daa18
zhangshoujie <1753141460@qq.com>
2020-10-01 15:23:30 +0800

    10.1submit

--- java-study/src/com/soft2036/unit3/ArrayForExample.java
+++ java-study/src/com/soft2036/unit3/ArrayForExample.java
@@ -1,7 +1,24 @@
 package com.soft2036.unit3;
 
+/**
+ * @author 张守杰
+ */
 public class ArrayForExample {
     public static void main(String[] args) {
-        
+        int a[] = {1,2,3,4};
+        char b[] = {'a','b','c','d'};
+        for (int n = 0;n < a.length;n++){
+            System.out.println(a[n]);
+        }
+        for (int n = 0;n < b.length;n++){
+            System.out.println(b[n]);
+        }
+        for (int i:a){
+            System.out.println(i);
+        }
+        for (char ch:b){
+            System.out.println(ch);
+        }
+
     }
 }
