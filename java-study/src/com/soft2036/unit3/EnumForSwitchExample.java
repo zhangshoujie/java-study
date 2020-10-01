225e946b8c181587fa65ed96a11be560f2e114e6
zhangshoujie <1753141460@qq.com>
2020-10-01 15:31:33 +0800

    10.1submit

---
+++ java-study/src/com/soft2036/unit3/EnumForSwitchExample.java
@@ -0,0 +1,30 @@
+package com.soft2036.unit3;
+enum Fruit{
+    苹果,梨,香蕉,西瓜,芒果
+        }
+/**
+ * @author 张守杰
+ */
+public class EnumForSwitchExample {
+    public static void main(String[] args) {
+        double price = 0;
+        boolean show = false;
+        for (Fruit fruit:Fruit.values()){
+            switch (fruit){
+                case 苹果:price = 1.5;
+                            show = true;
+                            break;
+                case 芒果:price = 6.8;
+                            show = true;
+                            break;
+                case 香蕉:price = 2.8;
+                            show = true;
+                            break;
+                default:show = false;
+            }
+            if (show){
+                System.out.println(fruit + "500克的价格:" + price + "元");
+            }
+        }
+    }
+}
