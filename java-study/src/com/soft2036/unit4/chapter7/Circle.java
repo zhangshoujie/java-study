f80b2a602112e79bf7e2d7215174596763f17741
zhangshoujie <1753141460@qq.com>
2020-10-02 16:20:15 +0800

    10.2submit

---
+++ java-study/src/com/soft2036/unit4/chapter7/Circle.java
@@ -0,0 +1,27 @@
+package com.soft2036.unit4.chapter7;
+
+public class Circle {
+    double x,y,radius;
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
---
+++ java-study/src/com/soft2036/unit4/chapter7/Rectangle.java
@@ -0,0 +1,32 @@
+package com.soft2036.unit4.chapter7;
+
+public class Rectangle {
+    double x,y,width,height;
+    public void setX(double a){
+        x = a;
+    }
+    public double getX(){
+        return x;
+    }
+    public void  setY(double b){
+        y = b;
+    }
+    public double getY(){
+        return y;
+    }
+    public void setWidth(double w){
+        if(width > 0)
+            width = w;
+    }
+    public double getWidth(){
+        return width;
+    }
+    public void setHeight(double h){
+        if (height > 0)
+            height = h;
+    }
+    public double getHeight(){
+        return height;
+    }
+
+}
