e34f214b4d60a2423b945606833e4248a275c32d
zhangshoujie <1753141460@qq.com>
2020-10-03 19:18:33 +0800

    10.3submit

---
+++ java-study/src/com/soft2036/unit4/Example4_19.java
@@ -0,0 +1,10 @@
+package com.soft2036.unit4;
+
+public class Example4_19 {
+    public static void main(String[] args) {
+        Integer x = 100,y = 12;
+        Integer m = x + y;
+        int ok = m;
+        System.out.println(ok);
+    }
+}
