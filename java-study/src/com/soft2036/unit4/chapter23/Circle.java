4650557f29cd8ca2d3ec763824fe58a02d229821
zhangshoujie <1753141460@qq.com>
2020-10-04 10:15:41 +0800

    10.4submit

---
+++ java-study/src/com/soft2036/unit4/chapter23/Circle.java
@@ -0,0 +1,11 @@
+package com.soft2036.unit4.chapter23;
+
+public class Circle {
+    double radius;
+    Circle(double r){
+        radius = r;
+    }
+    double getArea(){
+        return 3.14 * radius * radius;
+    }
+}
